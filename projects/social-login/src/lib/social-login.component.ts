f67abefe2b0427785846067086a8982ce9bb551e
Rishabh <grishabh1992@gmail.com>
2021-09-11 13:42:00 +0530

    application created

---
+++ projects/social-login/src/lib/social-login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-socialLogin',
+  template: `
+    <p>
+      social-login works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SocialLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
